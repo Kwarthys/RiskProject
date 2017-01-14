c0228f80d9c01179cf30b98037caf9ca9980414f
Kwarthys <Kwarthys@gmail.com>
2017-01-14 16:25:13 +0100

    Tank model added and script created

---
+++ Assets/Scripts/TankDriver.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankDriver : MonoBehaviour{
+
+    // Use this for initialization
+    private Transform gun, turret;
+
+	void Start () {
+        turret = this.gameObject.transform.Find("Turret");
+        gun = turret.Find("Gun");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
